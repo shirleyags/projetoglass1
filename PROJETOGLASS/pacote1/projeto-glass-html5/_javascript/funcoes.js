0c4b0af5cb760de15a0f4c1273f1ca18baaba9e0
Shirley Alice <41222167+shirleyags@users.noreply.github.com>
2018-08-08 00:05:06 -0300

    Projeto Glass - site

---
+++ PROJETOGLASS/pacote1/projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,15 @@
+        function mudaFoto (foto){
+            document.getElementById('iconeoculos').src=foto;
+        }
+        function mudaFoto2 (foto2){
+            document.getElementById('iconeespec').src=foto2;
+        }
+        function mudaFoto3 (foto3){
+            document.getElementById('iconefoto').src=foto3;
+        }
+        function mudaFoto4 (foto4){
+            document.getElementById('iconemulti').src=foto4;
+        }
+        function mudaFoto5 (foto5){
+            document.getElementById('iconecontato').src=foto5;
+        }
